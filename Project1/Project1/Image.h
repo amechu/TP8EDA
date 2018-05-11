40ca296f278e00b2fac0311154219229178a8198
Alan <31071359+amechu@users.noreply.github.com>
2018-05-11 15:17:20 -0300

    asf

--- Project1/Project1/Image.h
+++ Project1/Project1/Image.h
@@ -14,5 +14,8 @@ private:
 	unsigned height;
 	bool selected;
 	std::string name;
+	void setName();
+	void loadBitmap();
+	void destroyBitmap();
 };
 
---
+++ Project1/Project1/Page.cpp
@@ -0,0 +1,5 @@
+#include "Page.h"
+
+void Page::addImage(std::string path_, unsigned width_, unsigned height_) {
+	
+}
